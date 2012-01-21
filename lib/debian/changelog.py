0261dab1512cd034b845a335443ae573a460d304
Colin Watson <cjwatson@canonical.com>
2012-01-21 01:08:23 +0000

    Fix up debian.changelog for string handling changes in Python 3.

--- lib/debian/changelog.py
+++ lib/debian/changelog.py
@@ -30,6 +30,9 @@ import pwd
 import re
 import socket
 import warnings
+import sys
+
+import six
 
 from debian import debian_support
 
@@ -137,7 +140,7 @@ class ChangeBlock(object):
                 changes.append(change)
             self._changes = changes
 
-    def __unicode__(self):
+    def _format(self):
         # TODO(jsw): Switch to StringIO or a list to join at the end.
         block = ""
         if self.package is None:
@@ -170,8 +173,16 @@ class ChangeBlock(object):
             block += line + "\n"
         return block
 
-    def __str__(self):
-        return unicode(self).encode(self._encoding)
+    if sys.version >= '3':
+        __str__ = _format
+
+        def __bytes__(self):
+            return str(self).encode(self._encoding)
+    else:
+        __unicode__ = _format
+
+        def __str__(self):
+            return unicode(self).encode(self._encoding)
 
 topline = re.compile(r'^(\w%(name_chars)s*) \(([^\(\) \t]+)\)'
                      '((\s+%(name_chars)s+)+)\;'
@@ -268,7 +279,9 @@ class Changelog(object):
         
         state = first_heading
         old_state = None
-        if isinstance(file, basestring):
+        if isinstance(file, bytes):
+            file = file.decode(encoding)
+        if isinstance(file, six.string_types):
             # Make sure the changelog file is not empty.
             if len(file.strip()) == 0:
                 self._parse_error('Empty changelog file.', strict)
@@ -276,7 +289,7 @@ class Changelog(object):
 
             file = file.splitlines()
         for line in file:
-            if not isinstance(line, unicode):
+            if not isinstance(line, six.text_type):
                 line = line.decode(encoding)
             # Support both lists of lines without the trailing newline and
             # those with trailing newlines (e.g. when given a file object
@@ -468,15 +481,23 @@ class Changelog(object):
     def _raw_versions(self):
         return [block._raw_version for block in self._blocks]
 
-    def __unicode__(self):
+    def _format(self):
         pieces = []
-        pieces.append(u'\n'.join(self.initial_blank_lines))
+        pieces.append(six.u('\n').join(self.initial_blank_lines))
         for block in self._blocks:
-            pieces.append(unicode(block))
-        return u''.join(pieces)
+            pieces.append(six.text_type(block))
+        return six.u('').join(pieces)
 
-    def __str__(self):
-        return unicode(self).encode(self._encoding)
+    if sys.version >= '3':
+        __str__ = _format
+
+        def __bytes__(self):
+            return str(self).encode(self._encoding)
+    else:
+        __unicode__ = _format
+
+        def __str__(self):
+            return unicode(self).encode(self._encoding)
 
     def __iter__(self):
         return iter(self._blocks)
